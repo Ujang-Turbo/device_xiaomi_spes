fd34753d6cf2b210360d72a27c3d6ba5061d28e0
Agatha <naufalmarjan2014@gmail.com>
2023-03-06 11:20:50 +0000

    spes: Ricedroid

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -8,20 +8,26 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
-
 # Inherit from spes device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Nad Stuff
-NAD_BUILD_TYPE := OFFICIAL
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR := true
-USE_PIXEL_CHARGING := true
+# Inherit Ricedroid Stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := nad_spes
+# Ricedroid
+RICE_CHIPSET := SM689
+RICE_MAINTAINER := Agatha
+RICE_PACKAGE_TYPE := PIXEL
+SUSHI_BOOTANIMATION := 1080
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_ENABLE_BLUR := true
+
+# GMS build flags
+WITH_GMS := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+# Device identifier
+PRODUCT_NAME := lineage_spes
 PRODUCT_DEVICE := spes
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -30,7 +36,3 @@ PRODUCT_MODEL := Redmi Note 11
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Redmi/spes_global/spes:12/SKQ1.211103.001/V13.0.4.0.SGCMIXM:user/release-keys
-
-# GMS
-TARGET_GAPPS_ARCH := arm64
-USE_GAPPS := true
