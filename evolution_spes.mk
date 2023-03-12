013775bfded3093e15dffeb038e56db1d1374b3d
Agatha q <naufalmarjan2014@gmail.com>
2023-03-12 05:30:45 +0000

    spes: Evolution X time!

--- evolution_spes.mk
+++ evolution_spes.mk
@@ -8,20 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
-
 # Inherit from spes device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Nad Stuff
-NAD_BUILD_TYPE := OFFICIAL
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR := true
-USE_PIXEL_CHARGING := true
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := nad_spes
+# Evolution-X Stuff
+EVO_BUILD_TYPE := UNOFFICIAL
+TARGET_ENABLE_BLUR := true
+
+# Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+PRODUCT_NAME := evolution_spes
 PRODUCT_DEVICE := spes
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -30,7 +33,3 @@ PRODUCT_MODEL := Redmi Note 11
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Redmi/spes_global/spes:12/SKQ1.211103.001/V13.0.4.0.SGCMIXM:user/release-keys
-
-# GMS
-TARGET_GAPPS_ARCH := arm64
-USE_GAPPS := true
